c93ab83a4bee69eb02f9d6ee810ea867618d6a48
Jeremy Mates <jmates@cpan.org>
2014-05-02 03:23:22 +0000

    Signify.

--- lib/File/AtomicWrite.pm
+++ lib/File/AtomicWrite.pm
@@ -21,7 +21,7 @@ use File::Path qw/mkpath/;
 use File::Temp qw/tempfile/;
 use IO::Handle;
 
-our $VERSION = '1.14';
+our $VERSION = '1.15';
 
 # Default options
 my %default_params = ( MKPATH => 0, template => ".tmp.XXXXXXXXXX" );
